ea1366a76b95b7e6fa1d05f13856c05003a1a469
Tom Bredemeier <tbredemeier@barrington220.org>
2017-09-28 12:45:41 -0500

    Added collection view protocols

--- Snap Share/ViewController.swift
+++ Snap Share/ViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    var images = [UIImage]()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,17 @@ class ViewController: UICollectionViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        if let imageView = cell.viewWithTag(100) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+        return cell
+    }
 
 }
 
