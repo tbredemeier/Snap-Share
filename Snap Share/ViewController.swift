f34f25532641d3303774273e5472040a27c30e17
Tom Bredemeier <tbredemeier@barrington220.org>
2017-09-29 12:30:13 -0500

    Added image picker

--- Snap Share/ViewController.swift
+++ Snap Share/ViewController.swift
@@ -15,12 +15,27 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "Snap Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+    }
+
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            dismiss(animated: true, completion: nil)
+            images.insert(image, at: 0)
+            collectionView?.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
